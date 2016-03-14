e0f711e260cd1db871ae7649b47ec9270e71df70
josh <jwentwor@gmail.com>
2016-03-14 12:20:30 -0700

    passes half of specRunner

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,40 @@
+var init = function() {};
+
+var send = function(message) {
+  $.ajax({
+    url: '',
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify(message),
+    contentType: "application/json; charset=utf-8"
+  })
+};
+
+var fetch = function() {
+  $.ajax({
+    type: 'GET',
+    dataType: 'json'
+  })
+};
+
+var clearMessages  = function() {
+  $('#chats').empty();
+};
+
+var addMessage  = function(message) {
+  $('#chats').html('<blink>' + message['text'] + '</blink>');
+};
+
+var addRoom  = function() {};
+
+
+var app = {
+  init: init,
+  send: send,
+  fetch: fetch,
+  clearMessages: clearMessages,
+  addMessage: addMessage,
+  addRoom: addRoom
+}
 // YOUR CODE HERE:
 
