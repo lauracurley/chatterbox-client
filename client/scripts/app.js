e9ead84a0a4baedf627ee332c26a6f760fb7b9dc
lauracurley <info@lauracurley.com>
2016-03-14 15:52:04 -0700

    Pass spec runner up to events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,12 @@
-var init = function() {};
+
+var init = function() {
+  _.each(messages, function(message, i, messages) {
+    app.addMessage(message);
+  });
+
+};
+var friends = [];
+var messages = [];
 
 var send = function(message) {
   $.ajax({
@@ -6,26 +14,52 @@ var send = function(message) {
     type: 'POST',
     dataType: 'json',
     data: JSON.stringify(message),
-    contentType: "application/json; charset=utf-8"
-  })
+    contentType: 'application/json; charset=utf-8'
+  });
 };
 
-var fetch = function() {
+// app's fetch url - 'https://api.parse.com/1/classes/messages'
+
+var fetch = function(url) {
   $.ajax({
+    url: url,
     type: 'GET',
-    dataType: 'json'
-  })
+    dataType: 'json',
+    success: function(data) {
+      messages = data.results;
+      console.log(messages);
+    },
+    error: function(data) {
+      console.log('fetch request failed : ' + data.toString());
+    }
+  });
 };
 
-var clearMessages  = function() {
+var clearMessages = function() {
   $('#chats').empty();
 };
 
-var addMessage  = function(message) {
-  $('#chats').html('<blink>' + message['text'] + '</blink>');
+var addMessage = function(message) {
+  var $message = $('<div></div>');
+  $message.append('<h2 class="username">' + message['username'] + '</h2>');
+  $message.append('<p>' + message['text'] + '</p>');
+  $('#chats').append($message);
 };
 
-var addRoom  = function() {};
+var addRoom = function(roomName) {
+  $('#roomSelect').append('<div>' + roomName + '</div>');
+};
+
+var addFriend = function(userName) {
+  friends.push(userName);
+};
+
+//TODO: Make this filter stuff
+var filterMessages = function(data) {
+  _.each(data.results, function(message, i, list) {
+    messages.push(message);
+  });
+};
 
 
 var app = {
@@ -35,6 +69,6 @@ var app = {
   clearMessages: clearMessages,
   addMessage: addMessage,
   addRoom: addRoom
-}
+};
 // YOUR CODE HERE:
 
